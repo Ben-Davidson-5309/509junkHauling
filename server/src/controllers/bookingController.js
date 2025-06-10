daed9ebf2c15bfd298fa6045cc8d2719d26f23c9
Benjamin Davidsone <bendavidson5309@icloud.comm>
2025-06-10 13:20:36 -0700

    ficking name

--- server/src/controllers/bookingController.js
+++ server/src/controllers/bookingController.js
@@ -1,4 +1,4 @@
-const Booking = require('../models/Booking');
+const Booking = require('../models/BookingModel');
 const sendEmail = require('../utils/sendEmail');
 
 exports.getBookingForm = (req, res) => {
